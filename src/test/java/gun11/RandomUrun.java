22dde9448e763b152b792037f3a5e10c6979d9a2
atezgider <atezgider@gmail.com>
2022-03-28 22:23:24 +0200

    28.03

--- src/test/java/gun11/RandomUrun.java
+++ src/test/java/gun11/RandomUrun.java
@@ -3,12 +3,15 @@ package gun11;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.locators.RelativeLocator;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import utils.ParentClass;
 
 import java.util.List;
 import java.util.Random;
+import java.util.regex.Pattern;
 
 import static gun11.RandomUrunLocators.*;
 import static gun11.RandomUrunLocatorsClass.*;
@@ -18,6 +21,7 @@ public class RandomUrun extends ParentClass {
     List<WebElement> products;
     List<WebElement> productPrices;
     List<WebElement> productAddToCarts;
+    Object[] myUrun;
 
     @Test
     public void test1(){
@@ -40,13 +44,37 @@ public class RandomUrun extends ParentClass {
         }
 
         int rUrun = new Random().nextInt(productPrices.size());
-        Object[] myUrun = {products.get(rUrun).getText(), getPriceAsNumber(productPrices.get(rUrun).getText())};
+
+        myUrun = new Object[]{products.get(rUrun).getText(), getPriceAsNumber(productPrices.get(rUrun).getText())};
 
         productAddToCarts.get(rUrun).click();
 
 
+        wait.until(ExpectedConditions.not(ExpectedConditions.textMatches(lCart, Pattern.compile(".*Loading.*"))));
+        driver.findElement(lCart).click();
+
+        //wait.until(ExpectedConditions.elementToBeClickable(lCart)).click();
+        WebElement cartPanel = wait.until(ExpectedConditions.visibilityOfElementLocated(lCartPanel));
+
+        Assert.assertTrue(cartPanel.getText().contains(myUrun[0].toString()));
+        Assert.assertTrue(cartPanel.getText().contains(products.get(rUrun).getText()));
+
     }
 
+
+    @Test
+    public void test3(){
+         String urunAdi = driver.findElement(lCartTableUrunIsimleri).getText();
+         String urunFiyati = driver.findElement(lCartTotal).getText();
+         String urunFiyati1 = driver.findElement(RelativeLocator.with(By.cssSelector("td")).toRightOf(lCartTotalText)).getText();
+
+         Assert.assertEquals(urunAdi, myUrun[0]);
+         Assert.assertEquals(getPriceAsNumber(urunFiyati), myUrun[1]);
+         Assert.assertEquals(getPriceAsNumber(urunFiyati1), myUrun[1]);
+
+
+
+    }
 
 
     public double getPriceAsNumber(String text){
@@ -63,6 +91,14 @@ interface RandomUrunLocators {
     By lProductNames = By.cssSelector(".caption a");
     By lProductPrices = By.xpath("//div[@class='caption']/p[2]");
     By lProductAddToCarts = By.xpath("//button[contains(@onclick,'cart.add')]");
+    By lCart = By.cssSelector("#cart button");
+    By lCartPanel = By.cssSelector("ul.dropdown-menu.pull-right");
+
+    By lCartTableUrunIsimleri = By.xpath("//table[@class='table table-striped']//td[2]");
+    By lCartTotal = By.xpath("//table[@class='table table-bordered']//tr[4]/td[2]");
+    By lCartTotalText = By.xpath("//td[./*[text()='Total']]");
+
+
 }
 
 class RandomUrunLocatorsClass {
